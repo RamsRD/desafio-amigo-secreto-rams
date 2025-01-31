4a219a4f1de2ace89408c64b38be6276312ce206
RamsRD <ramsesruedadelgado@gmail.com>
2025-01-31 15:07:40 -0400

    Inicio de JS

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+// Variables
 
+let listaDeAmigos = [];
+
+
+//Funciones
+
+function 
+
+
+
+// Consola (eliminar cuando se termine el proyecto)
+
+console.log('Ver lista de amigos:', listaDeAmigos);
